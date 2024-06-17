0b1dc9b920cbb2d1823ef8e5a90e25316e8da7a0
Thomas Dreibholz <dreibh@simula.no>
2024-06-17 16:08:00 +0200

    Improved example.

--- src/example.cc
+++ src/example.cc
@@ -1,28 +1,48 @@
+// Build Tool Example
+// Copyright (C) 2024 by Thomas Dreibholz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Contact: thomas.dreibholz@gmail.com
+
+#include "library.h"
+#include "version.h"
+
+#include <string.h>
 #include <iomanip>
 #include <iostream>
-
-
-// ###### Set ANSI color in terminal ########################################
-void setColor(std::ostream& os, unsigned int r, unsigned int g, unsigned int b)
-{
-   os << "\x1b[38;2;" << std::dec
-      << r << ";" << g << ";" << b << "m";
-}
-
-
-// ###### Reset ANSI color in terminal ######################################
-void resetColor(std::ostream& os)
-{
-    os << "\x1b[0m";
-}
-
 
 
 // ###### Main program ######################################################
 int main(int argc, char** argv)
 {
+   // ====== Handle arguments ===============================================
+   for(int i = 1; i < argc; i++) {
+      if( (!(strcmp(argv[i], "-v"))) || (!(strcmp(argv[i], "--version"))) ) {
+         std::cout << EXAMPLE_PACKAGE << "\n";
+         return 0;
+      }
+      else {
+         std::cerr << "Usage: " << argv[0] << " [--version|-v]\n";
+         exit(1);
+      }
+   }
+
+   // ====== Hello World! ===================================================
    std::cout << "This is a test!\n";
 
+   // ====== Some ANSI color tests ==========================================
    std::cout.fill('0');
    for(unsigned int r = 0; r < 256; r += 16) {
       setColor(std::cout, r, 0, 0);
